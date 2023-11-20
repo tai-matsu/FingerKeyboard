d771b02e62b4f139977831ddf08fa46ca19673c8
tai-matsu <109422211+tai-matsu@users.noreply.github.com>
2023-11-20 12:19:50 +0900

    speech追加

---
+++ Assets/Scrpits/Speech/SpeechToText.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.CognitiveServices.Speech;
+using System;
+
+public class SpeechToText : MonoBehaviour
+{
+
+    [SerializeField] private string subscriptionKey;
+    [SerializeField] private string region;
+    [SerializeField] private string speakText;
+
+    private SpeechConfig config;
+    private string speechSynthesisLanguage = "ja-JP";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        config = SpeechConfig.FromSubscription(subscriptionKey, region);
+        config.SpeechSynthesisLanguage = speechSynthesisLanguage;
+        config.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Raw16Khz16BitMonoPcm);
+    }
+
+
+    public async void SynthesizeAudioAsync()
+    {
+        try
+        {
+            using var synthesizer = new SpeechSynthesizer(config, null);
+            var result = await synthesizer.SpeakTextAsync(speakText);
+
+            var audioSource = gameObject.AddComponent<AudioSource>();
+            var sampleCount = result.AudioData.Length / 2;
+            var audioData = new float[sampleCount];
+            for (var i = 0; i < sampleCount; ++i)
+            {
+                audioData[i] = (short)(result.AudioData[i * 2 + 1] << 8 | result.AudioData[i * 2]) / 32768.0F;
+            }
+
+            var audioClip = AudioClip.Create("SynthesizedAudio", sampleCount, 1, 16000, false);
+            audioClip.SetData(audioData, 0);
+            audioSource.clip = audioClip;
+            audioSource.Play();
+
+            Debug.Log("AudioSynthesized");
+
+        }
+        catch(Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+    }
+}
